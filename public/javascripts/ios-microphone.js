2ed2da13384dc18569bd66dfb348b603d4bffde9
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-07-20 06:45:57 +0900

    Add code to check whether microphone usable

--- public/javascripts/ios-microphone.js
+++ public/javascripts/ios-microphone.js
@@ -1,1 +1,38 @@
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+window.URL = window.URL || window.webkitURL;
+window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+
 var mic = document.getElementById("microphone");
+mic.addEventListener("change", function(event) {
+    var fileList = event.target.files;
+    var audioFile = null;
+    for (var i = 0; i < fileList.length; i++) {
+        var file = fileList[i];
+        if (file.type.indexOf("audio/") === 0) {
+            audioFile = file;
+            break;
+        }
+    }
+    if (! audioFile) {
+        return;
+    }
+    var reader = new FileReader();
+    reader.addEventListener("load", function(event) {
+        var audioContext = new AudioContext();
+        audioContext.decodeAudioData(
+
+            event.target.result,
+
+            function(buffer) {
+                alert("OK");
+            },
+
+            function(error) {
+                alert(error);
+            }
+
+        );
+    });
+
+    reader.readAsArrayBuffer(audioFile);
+});
