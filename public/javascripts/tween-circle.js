a1679c404d397fdd8809c73390a2114856739ab4
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-07-19 22:08:01 +0900

    時間差をつけたトゥウィーン

--- public/javascripts/tween-circle.js
+++ public/javascripts/tween-circle.js
@@ -2,21 +2,32 @@ cj = createjs;
 
 document.addEventListener("DOMContentLoaded", function() {
     var stageElem = document.getElementById("stage");
+    stageElem.width = window.innerWidth;
+    stageElem.height = window.innerHeight;
     var stage = new cj.Stage(stageElem);
-    var circle = new cj.Shape();
-    var graphics = circle.graphics;
+    var circles = [];
+    var circleCount = 20;
+    var delay = 1 / circleCount;
+    var centerPoint = {x: stageElem.width / 2, y:stageElem.height / 2};
 
-    graphics.setStrokeStyle(10);
-    graphics.beginStroke("#113355");
-    graphics.drawCircle(0, 0, 20);
+    for (var i = 0; i < circleCount; i++) {
+        var circle = new cj.Shape();
+        var graphics = circle.graphics;
+        graphics.setStrokeStyle(1);
+        graphics.beginStroke("#113355");
+        graphics.drawCircle(0, 0, (i + 1) * 6);
 
-    circle.x = stageElem.width * Math.random();
-    circle.y = stageElem.height * Math.random();
-    cj.Tween.get(circle).to({x: stageElem.width / 2, y: stageElem.height / 2}, 1500, cj.Ease.bouseOut)
+        circle.x = stageElem.width * Math.random();
+        circle.y = stageElem.height * Math.random();
 
-    stage.addChild(circle);
+        cj.Tween.get(circle).to(centerPoint, 1500, cj.Ease.bounceOut);
+        circles.push(circle);
+        stage.addChild(circle);
+    }
     stage.addEventListener("stagemouseup", function(event) {
-        cj.Tween.get(circle).to({x: stage.mouseX, y: stage.mouseY}, 1500, cj.Ease.bounseOut)
+        circles.forEach(function(circle, i) {
+            cj.Tween.get(circle).to({x: stage.mouseX, y: stage.mouseY}, (0.5 + i * delay) * 1500, cj.Ease.bounceOut)
+        });
     });
 
     cj.Ticker.addEventListener("tick", stage);
