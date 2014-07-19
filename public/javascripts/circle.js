0177043753ebe2775955948f7041ffab69dfc69a
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-07-20 01:30:16 +0900

    Add circle.html

---
+++ public/javascripts/circle.js
@@ -0,0 +1,28 @@
+var cj = createjs;
+
+document.addEventListener("DOMContentLoaded", function() {
+    var canvas = document.getElementById("stage");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    var longSide = Math.max(canvas.width, canvas.height);
+    var shortSide = Math.min(canvas.width, canvas.height);
+    var origin = {x: canvas.width / 2, y: canvas.height /2}
+    var stage = new cj.Stage(canvas);
+    var outerCircle = new cj.Shape();
+    var innerCircle = new cj.Shape();
+
+    [outerCircle, innerCircle].forEach(function(circle) {
+	circle.graphics.beginStroke("#ffffff");
+	circle.set(origin);
+	stage.addChild(circle);
+    });
+
+    outerCircle.graphics.setStrokeStyle(12);
+    outerCircle.graphics.drawCircle(0, 0, shortSide * 0.6 / 2);
+
+    var radius = shortSide * 0.4 / 2;
+    innerCircle.graphics.setStrokeStyle(radius);
+    innerCircle.graphics.drawCircle(0, 0, radius / 2);
+
+    stage.update();
+});
