25744a7a2a18919601ed2a52f19097a56f6ceb5d
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-07-20 18:45:57 +0900

    Integrate two concentric circles into one shape

--- public/javascripts/circle.js
+++ public/javascripts/circle.js
@@ -6,21 +6,19 @@ document.addEventListener("DOMContentLoaded", function() {
     var shortSide = Math.min(canvas.width, canvas.height);
     var origin = {x: canvas.width / 2, y: canvas.height /2}
     var stage = new Stage(canvas);
-    var outerCircle = new Shape();
-    var innerCircle = new Shape();
+    var circle = new Shape();
 
-    [outerCircle, innerCircle].forEach(function(circle) {
-	circle.shadow = new Shadow("#999999", 6, 10, 24);
-	circle.set(origin);
-	stage.addChild(circle);
-    });
-
-    outerCircle.graphics.beginStroke("#ffffff");
-    outerCircle.graphics.setStrokeStyle(12);
-    outerCircle.graphics.drawCircle(0, 0, shortSide * 0.6 / 2);
-
-    innerCircle.graphics.beginFill("#ffffff");
-    innerCircle.graphics.drawCircle(0, 0, shortSide * 0.4 / 2);
+    circle.graphics
+	.beginStroke("#ffffff")
+        .setStrokeStyle(12)
+        .drawCircle(0, 0, shortSide * 0.6 / 2)
+        .endStroke()
+        .beginFill("#ffffff")
+        .drawCircle(0, 0, shortSide * 0.4 / 2)
+        .endFill();
+    circle.shadow = new Shadow("#999999", 6, 10, 24);
+    circle.set(origin);
+    stage.addChild(circle);
 
     stage.update();
 });
