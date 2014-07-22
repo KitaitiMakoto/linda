a9cf0c124eabb9c34c1b778bc5440346a9bb1dd3
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-07-22 14:09:24 +0900

    Draw graph from audio files

--- public/javascripts/ios-microphone.js
+++ public/javascripts/ios-microphone.js
@@ -25,6 +25,45 @@ mic.addEventListener("change", function(event) {
 
             function(buffer) {
                 alert("OK");
+                var source = audioContext.createBufferSource();
+                source.buffer = buffer;
+                var filter = audioContext.createBiquadFilter();
+                filter.type = 'lowpass';
+                filter.frequency.value = 1000;
+
+                analyser = audioContext.createAnalyser();
+                analyser.fftSize = 1024;
+                analyser.smoothingTimeContent = 0.9;
+
+                source.connect(filter);
+                filter.connect(analyser);
+
+                var freqContext = document.getElementById("frequency").getContext("2d");
+                var tdContext = document.getElementById("timedomain").getContext("2d");
+                var data = new Uint8Array(256);
+                window.setInterval(function() {
+
+                    analyser.getByteFrequencyData(data);
+
+                    freqContext.fillStyle = "#ffffff";
+                    freqContext.fillRect(0, 0, 256, 256);
+                    for (var i = 0; i < 256; i++) {
+                        freqContext.fillStyle = "#000000";
+                        freqContext.fillRect(i, 256 - data[i], 1, data[i]);
+                    }
+
+                    analyser.getByteTimeDomainData(data);
+                    tdContext.fillStyle = "#ffffff";
+                    tdContext.fillRect(0, 0, 256, 256);
+                    for (var i = 0; i < 256; i++) {
+                        tdContext.fillStyle = "#000000";
+                        tdContext.fillRect(i, 256 - data[i], 1, data[i]);
+                    }
+
+                }, 100);
+
+                source.noteOn(0);
+
             },
 
             function(error) {
