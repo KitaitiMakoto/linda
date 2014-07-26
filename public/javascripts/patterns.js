22c6917e31c18fb207e9725b4eade02da098a24f
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-07-27 01:53:30 +0900

    Translate original point as (0, 0)

--- public/javascripts/patterns.js
+++ public/javascripts/patterns.js
@@ -4,17 +4,20 @@ document.addEventListener("DOMContentLoaded", function() {
     canvas.height = window.innerHeight;
     var shortSide = Math.min(canvas.width, canvas.height);
     var stage = new Stage(canvas);
-    var circle = new Linda.Circle(stage, {x: canvas.width / 2, y: canvas.height / 2, radius: shortSide * 0.6 / 2});
+    stage.getMatrix()
+        .translate(canvas.width / 2, canvas.height / 2)
+        .decompose(stage);
+    var circle = new Linda.Circle(stage, {radius: shortSide * 0.6 / 2});
 
     Ticker.addEventListener("tick", stage);
 
     circle
         .draw()
-        .moveTo({x: 150, y: 150}, 1000, function() {
+        .moveTo({x: -300, y: -100}, 1000, function() {
             this.moveBy({x: 150}, 100, function() {
                 this.scaleTo({scaleX: 0.6, scaleY: 0.6}, 800, function() {
 
-                    this.tweenTo({x: canvas.width / 2, y: canvas.height / 2, scaleX: 1, scaleY: 1}, 100);
+                    this.tweenTo({x: 0, y: 0, scaleX: 1, scaleY: 1}, 100);
                 });
             });
         });
