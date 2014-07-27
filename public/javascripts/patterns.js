87aec801f4977878cecfc6963cad376fc6964fba
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-07-27 09:15:00 +0900

    Construct tween chain from UI

--- public/javascripts/patterns.js
+++ public/javascripts/patterns.js
@@ -32,4 +32,39 @@ document.addEventListener("DOMContentLoaded", function() {
             }
             return promise;
         });
+
+    var tweens = new Vue({
+        el: "#tweens",
+        data: {
+            tweens: []
+        },
+        methods: {
+            remove: function(event) {
+                this.tweens.splice(event.targetVM.$index, 1);
+            }
+        },
+        created: function() {
+            this.tweens.push(
+                {func: "moveTo", args: [{x: -0.3, y: -0.1}, 1000]},
+                {func: "moveBy", args: [{x: 0.15}, 100]},
+                {func: "scaleTo", args: [{scaleX: 0.6, scaleY: 0.6}, 800]},
+                {func: "tweenTo", args: [{x: 0, y:0, scaleX: 1, scaleY: 1}, 100]}
+            );
+            for (var i = 0; i < 3; i++) {
+                this.tweens.push(
+                    {func: "disappear", args: [100]},
+                    {func: "appear", args: [100]}
+                );
+            }
+        }
+    });
+
+    window.run = function() {
+        var promise = circle.draw();
+        tweens.$data.tweens.forEach(function(tween) {
+            promise = promise.then(function(circle) {
+                return Linda.Circle.prototype[tween.func].apply(circle, tween.args);
+            });
+        });
+    }
 });
