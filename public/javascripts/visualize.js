94910a1e3c0cac4a8119f9f0e7bb879b2c84661c
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-07-29 21:33:56 +0900

    Add visualize.html

---
+++ public/javascripts/visualize.js
@@ -0,0 +1,45 @@
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+
+canvas = document.getElementById("stage");
+var WIDTH = canvas.width;
+var HEIGHT = canvas.height;
+
+navigator.getUserMedia(
+    {audio: true},
+    function(stream) {
+        var acon = new AudioContext();
+        var input = acon.createMediaStreamSource(stream);
+        var filter = acon.createBiquadFilter();
+        filter.frequency.value = 60.0;
+        filter.type = filter.NOTCH;
+        filter.Q = 10.0;
+        var analyser = acon.createAnalyser();
+        input.connect(filter);
+        filter.connect(analyser);
+
+        var ccon = canvas.getContext("2d");
+
+        var requestID = null;
+        requestID = requestAnimationFrame(function(timestamp) {
+            var freqDomain = new Uint8Array(analyser.frequencyBinCount);
+            analyser.getByteFrequencyData(freqDomain);
+            ccon.clearRect(0, 0, WIDTH, HEIGHT);
+            for (var i = 0; i < analyser.frequencyBinCount; i++) {
+                var value = freqDomain[i];
+                var percent = value / 256;
+                var height = HEIGHT * percent;
+                var offset = HEIGHT - height;
+                var barWidth = WIDTH / analyser.frequencyBinCount;
+                var hue = i / analyser.frequencyBinCount * 360;
+                ccon.fillStyle = "hsl(" + hue + ",100%, 50%)";
+                ccon.fillRect(i * barWidth, offset, barWidth, height);
+            }
+
+            requestID = requestAnimationFrame(arguments.callee);
+        });
+    },
+    function(error) {
+        alert(error);
+    }
+);
