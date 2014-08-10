77f0889ca6f4be01286bcdda566136a85f9a3bb3
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-08-10 20:31:31 +0900

    Add Linda.initializeStage() function

--- public/javascripts/linda.js
+++ public/javascripts/linda.js
@@ -1,1 +1,11 @@
-Linda = {};
+Linda = {
+    initializeStage: function(canvasElem) {
+        canvasElem.width = window.innerWidth;
+        canvasElem.height = window.innerHeight;
+        var stage = new Stage(canvasElem);
+        stage.getMatrix()
+            .translate(canvasElem.width / 2, canvasElem.height / 2)
+            .decompose(stage);
+        return stage;
+    }
+};
