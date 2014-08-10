207eb51fbdc722e2e7e53d1067dbf5ef36e93344
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-08-10 21:52:40 +0900

    Narrow radius down

--- public/javascripts/expanding-circles.js
+++ public/javascripts/expanding-circles.js
@@ -6,7 +6,8 @@ document.addEventListener("DOMContentLoaded", function() {
     var circles = [new Shape(), new Shape(), new Shape()];
     var circle = new Shape();
 
-    var scaleTo = (longSide * 1.5 / 2) / (shortSide * 0.5 / 2);
+    var radius = shortSide * 0.2 / 2;
+    var scaleTo = (longSide * 2 / 2) / radius;
 
     var run = function() {
         circles.forEach(function(circle, i) {
@@ -14,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 circle.graphics
                     .beginStroke("#cccccc")
                     .setStrokeStyle(2)
-                    .drawCircle(0, 0, shortSide * 0.5 / 2)
+                    .drawCircle(0, 0, radius)
                     .endStroke();
                 circle.set({x: 0, y: 0, scaleX: 1, scaleY: 1, alpha: 1})
                 stage.addChild(circle);
