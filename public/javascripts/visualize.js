1cfffe189f3976d96d296d7fd4139a8ee544fd94
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-08-12 14:19:33 +0900

    Expand volume range

--- public/javascripts/visualize.js
+++ public/javascripts/visualize.js
@@ -11,7 +11,7 @@ navigator.getUserMedia(
         var acon = new AudioContext();
         var input = acon.createMediaStreamSource(stream);
         var analyser = acon.createAnalyser();
-        analyser.maxDecibels = -80;
+        analyser.maxDecibels = -77;
         analyser.minDecibels = -90;
         input.connect(analyser);
 
