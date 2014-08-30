92b7316f9de952e954b7cff40cf057d7af19b7ec
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-08-31 02:30:15 +0900

    Fix indentation

--- public/javascripts/linda.js
+++ public/javascripts/linda.js
@@ -1,16 +1,16 @@
 Linda = {
     initializeStage: function(canvasElem, translate) {
-	if (arguments.length < 2) {
-	    translate = true;
-	}
+        if (arguments.length < 2) {
+            translate = true;
+        }
         canvasElem.width = window.innerWidth;
         canvasElem.height = window.innerHeight;
         var stage = new Stage(canvasElem);
-	if (translate) {
-        stage.getMatrix()
-            .translate(canvasElem.width / 2, canvasElem.height / 2)
-            .decompose(stage);
-	}
+        if (translate) {
+            stage.getMatrix()
+                .translate(canvasElem.width / 2, canvasElem.height / 2)
+                .decompose(stage);
+        }
         return stage;
     }
 };
