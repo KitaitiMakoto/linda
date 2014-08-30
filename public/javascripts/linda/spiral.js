47800bcbd5a15fbeddddb7e26de80c856a9b26ae
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-08-31 06:10:39 +0900

    Implement Linda.Spiral from prototype

---
+++ public/javascripts/linda/spiral.js
@@ -0,0 +1,22 @@
+Linda.Spiral = function(options) {
+    options = options || {};
+    this.rotation = options.rotation;
+    this.image = options.image;
+    this.thickness = options.thickness || 24;
+    this.x = options.x || 0;
+    this.y = options.y || 0;
+    this.shape = new Shape();
+};
+Linda.Spiral.prototype.draw = function(additionalAngle) {
+    this.shape.graphics
+        .clear()
+        .beginBitmapStroke(this.image)
+        .setStrokeStyle(this.thickness);
+    // stolen from http://stackoverflow.com/questions/6824391/drawing-a-spiral-on-an-html-canvas-using-javascript
+    for (i = 0; i < 720; i++) {
+        var angle = 0.1 * i;
+        var x = (1 + angle) * Math.cos(angle + additionalAngle) * 12;
+        var y = (1 + angle) * Math.sin(angle + additionalAngle) * 12;
+        this.shape.graphics.lineTo(this.x + x, this.y + y);
+    }
+};
