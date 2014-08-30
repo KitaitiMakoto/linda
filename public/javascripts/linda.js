6d8b75e4d4519f663826345c0ea6ab7478d662f8
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-08-31 01:09:48 +0900

    Make translate coordinate or not configurable

--- public/javascripts/linda.js
+++ public/javascripts/linda.js
@@ -1,11 +1,16 @@
 Linda = {
-    initializeStage: function(canvasElem) {
+    initializeStage: function(canvasElem, translate) {
+	if (arguments.length < 2) {
+	    translate = true;
+	}
         canvasElem.width = window.innerWidth;
         canvasElem.height = window.innerHeight;
         var stage = new Stage(canvasElem);
+	if (translate) {
         stage.getMatrix()
             .translate(canvasElem.width / 2, canvasElem.height / 2)
             .decompose(stage);
+	}
         return stage;
     }
 };
