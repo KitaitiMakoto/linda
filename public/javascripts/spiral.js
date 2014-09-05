1421d5e266fc647d85c0e2e70aba0b7c9d781f1a
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-09-05 13:38:05 +0900

    rel="import"をなくしたのでロードを待つ必要がなくなっていた

--- public/javascripts/spiral.js
+++ public/javascripts/spiral.js
@@ -1,24 +1,21 @@
 window.addEventListener("DOMContentLoaded", function() {
-    var linda = document.getElementById("linda");
-    linda.addEventListener("load", function() {
-        var canvas = document.getElementById("stage");
-        var stage = Linda.initializeStage(canvas, false);
-        var origin = {x: canvas.width / 2, y: canvas.height / 2};
-        var queue = new LoadQueue();
-        queue.on("fileload", function(event) {
-            var rotation = 24*Math.PI;
-            var startedAt = null;
-            var duration = 6000;
+    var canvas = document.getElementById("stage");
+    var stage = Linda.initializeStage(canvas, false);
+    var origin = {x: canvas.width / 2, y: canvas.height / 2};
+    var queue = new LoadQueue();
+    queue.on("fileload", function(event) {
+        var rotation = 24*Math.PI;
+        var startedAt = null;
+        var duration = 6000;
 
-            Ticker.addEventListener("tick", stage);
+        Ticker.addEventListener("tick", stage);
 
-            var spiral = new Linda.SpiralCircle(event.result, {
-                x: origin.x,
-                y: origin.y
-            });
-            stage.addChild(spiral.shape);
-            spiral.animate(rotation, duration);
+        var spiral = new Linda.SpiralCircle(event.result, {
+            x: origin.x,
+            y: origin.y
         });
-        queue.loadFile("images/bg_sea01.jpg");
+        stage.addChild(spiral.shape);
+        spiral.animate(rotation, duration);
     });
+    queue.loadFile("images/bg_sea01.jpg");
 });
