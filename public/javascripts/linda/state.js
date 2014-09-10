ec7b3011afc999e0175f1023c7ba6fb78dd41ea7
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-09-11 00:40:26 +0900

    Remove Linda.State.View.update

--- public/javascripts/linda/state.js
+++ public/javascripts/linda/state.js
@@ -4,6 +4,9 @@ Linda.State = function() {
 };
 Linda.State.prototype.set = function(state) {
     var oldState = this.state;
+    if (oldState === "animating" && state !== "listening") {
+        return;
+    }
     this.state = state;
     this.observers.forEach(function(observer) {
         observer(state, oldState);
@@ -11,14 +14,12 @@ Linda.State.prototype.set = function(state) {
 };
 Linda.State.View = function(element, state) {
     this.element = element;
-    state.observers.push(this.update.bind(this));
+    state.observers.push(this.render.bind(this));
 };
-Linda.State.View.prototype.update = function(state, oldState) {
-    if (state !== oldState) {
-        this.render(state);
+Linda.State.View.prototype.render = function(state, oldState) {
+    if (state === oldState) {
+        return;
     }
-};
-Linda.State.View.prototype.render = function(state) {
     this.element.setAttribute("class", state);
     this.element.textContent = state;
 };
