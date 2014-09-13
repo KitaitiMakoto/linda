b22398b75f79ace9650c5d3b50acbc991b363be6
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-09-14 05:22:32 +0900

    Add spots animation

---
+++ public/javascripts/linda/spots.js
@@ -0,0 +1,15 @@
+Linda.Spots = function() {};
+Linda.Animation.patterns.push(Linda.Spots);
+Linda.Spots.draw = function(progress) {
+    var canvas = this.shape.parent.canvas;
+    var x = Math.random() * canvas.width;
+    var y = Math.random() * canvas.height;
+    var radius = Math.random() * canvas.width / 12;
+    var graphics = (progress === 0)
+                 ? this.clear()
+                 : this.shape.graphics;
+    graphics
+        .beginBitmapFill(this.image)
+        .drawCircle(x, y, radius)
+        .endFill();
+};
