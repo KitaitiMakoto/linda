e1add15cf2b322393a0d03044d5f53fd8294ed21
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-09-14 22:34:17 +0900

    Add SpotLight

---
+++ public/javascripts/linda/spot_light.js
@@ -0,0 +1,16 @@
+Linda.SpotLight = {};
+Linda.Animation.patterns.push(Linda.SpotLight);
+Linda.SpotLight.draw = function(progress) {
+    progress = Math.min(progress, 1);
+    var canvas = this.shape.parent.canvas;
+    var canvasWidth = canvas.width;
+    var radius = canvasWidth / 2 * (1 - progress);
+    var gradient = 2 * Math.PI * progress;
+    this.beginDrawing()
+        .setStrokeStyle(0)
+        .beginBitmapFill(this.image)
+        .arc(this.x, this.y, radius, gradient, gradient + Math.PI / 6)
+        .lineTo(this.x, this.y)
+        .closePath()
+        .endFill();
+};
