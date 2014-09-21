f872bc62ae29e2c36975beb76a24ad3243c9039a
KitaitiMakoto <KitaitiMakoto@gmail.com>
2014-09-21 21:20:09 +0900

    [REFACTORING]Make more promise like

--- public/javascripts/linda/splash.js
+++ public/javascripts/linda/splash.js
@@ -1,17 +1,24 @@
 Linda.Splash = function() {};
 Linda.Splash.run = function() {
-    return Promise.all(Array.prototype.map.call(
-        document.querySelectorAll("#splash span"),
-        function(character, index) {
-            return new Promise(function(resolve, reject) {
-                setTimeout(function() {
-                    setTimeout(function() {
-                        character.addEventListener("transitionend", resolve);
-                        character.addEventListener("webkitTransitionEnd", resolve);
-                        character.classList.add("splashed");
-                    }, index * 300);
-                }, 800);
-            })
-        }
-    ));
+    return (new Promise(function(resolve, reject) {
+        setTimeout(resolve, 800);
+    })).then(function() {
+        var characters = document.querySelectorAll("#splash span");
+        characters = Array.prototype.slice.call(characters, 0, characters.length - 1);
+        return characters.reduce(function(sequence, character, index) {
+            return sequence.then(function() {
+                character.classList.add("splashed");
+                return new Promise(function(resolve, reject) {
+                    setTimeout(resolve, 300);
+                });
+            });
+        }, Promise.resolve())
+    }).then(function() {
+        return new Promise(function(resolve, reject) {
+            var character = document.querySelector("#splash span:last-child");
+            character.addEventListener("transitionend", resolve);
+            character.addEventListener("webkitTransitionEnd", resolve);
+            character.classList.add("splashed");
+        });
+    });
 };
