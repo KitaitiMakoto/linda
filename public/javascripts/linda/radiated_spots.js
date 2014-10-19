963f2329f111d87ad8b3c73396b87060febd304e
KitaitiMakoto <KitaitiMakoto@gmail.com>
2014-10-19 20:12:27 +0900

    Add RadiatedSpots animation pattern

---
+++ public/javascripts/linda/radiated_spots.js
@@ -0,0 +1,20 @@
+Linda.RadiatedSpots = {};
+Linda.Animation.patterns.push(Linda.RadiatedSpots);
+Linda.RadiatedSpots.draw = function(progress) {
+    var radius = 12;console.warn("parameterize circle radius");
+    var distance = 24;console.warn("parameterize circle distance");
+    var gradient = -2 * Math.PI * progress;
+    var graphics = this.beginDrawing()
+        .setStrokeStyle(0)
+        .beginBitmapFill(this.image)
+        .drawCircle(this.x, this.y, radius);
+    for (var i = 0; i < 8; i++) {
+        var angle = i * Math.PI / 4;
+        for (var j = 0; j < 12; j++) {
+            var coord = new Point((radius * 2 + distance) * (j + 1), 0);
+            coord = this.translate(this.rotate(this.rotate(coord, angle), gradient));
+            graphics.beginBitmapFill(this.image)
+                .drawCircle(coord.x, coord.y, radius);
+        }
+    }
+};
