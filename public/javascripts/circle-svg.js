56586f4a2f7dd1df88907ebf0c3d08a04989218b
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-11-04 00:46:44 +0900

    Remove prototype code

--- public/javascripts/circle-svg.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-window.addEventListener("load", function() {
-    var stage = document.getElementById("stage");
-    var group = document.getElementById("circle");
-    var origin = {x: window.innerWidth / 2, y: window.innerHeight / 2}
-    group.setAttribute("transform", "translate(" + origin.x + ", " + origin.y + ")");
-});
