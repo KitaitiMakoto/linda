f76e139b9bed27992f946605af11f762e70a1490
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-02-01 22:34:22 +0900

    Remove obsolete function

--- public/javascripts/linda.js
+++ public/javascripts/linda.js
@@ -195,19 +195,3 @@ Linda.adaptImage = function(image, width, height) {
     adaptedImage.src = canvas.toDataURL();
     return adaptedImage;
 };
-
-// Obsolete
-Linda.initializeStage = function(canvasElem, translate) {
-    if (arguments.length < 2) {
-        translate = true;
-    }
-    canvasElem.width = window.innerWidth;
-    canvasElem.height = window.innerHeight;
-    var stage = new Stage(canvasElem);
-    if (translate) {
-        stage.getMatrix()
-            .translate(canvasElem.width / 2, canvasElem.height / 2)
-            .decompose(stage);
-    }
-    return stage;
-};
